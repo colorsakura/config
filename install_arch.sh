95c2a8d0aa39d266ed5ae08be1b773872765af3f
chauncey <2268136725@qq.com>
2022-05-12 18:28:04 +0800

    update

--- install_arch.sh
+++ install_arch.sh
@@ -1,13 +1,11 @@
 #!/bin/zsh
 
-
 # Install Arch Linux
 
 # Install base packages and tools
-func install_base() {
-    pacman -Syu
-    pacman -S --noconfirm --needed base-devel git dhcpcd iwd vim sudo zsh zsh-completions zsh-autosuggestions zsh-syntax-highlighting \
-                openssh neofetch bluz bluz-utils xorg-server xorg-xinit xorg-xrandr xorg-xset  \
-                i3-gaps i3status-rust i3lock i3blocks i3blocks-color-git \    
+func install_base_pkg() {
+    echo "install base packages"
+    
+    pacman -S --noconfirm --needed base base-devel vim sudo zsh zsh-completions zsh-autosuggestions zsh-syntax-highlighting
 }
   
